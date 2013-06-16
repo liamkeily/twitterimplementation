e78506eb12b6a7d1efc3c8fe6b5d4b4e545fb1d6
liamkeily <liam@simplywebsites.com>
2013-06-16 13:27:34 +0100

    Created this because it took me ages to find out how to get a users latest tweet. Since the old API has been removed.

---
+++ implem.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'twitteroauth/twitteroauth.php';
+$twitterConnection = new TwitterOAuth(
+'XXX',	// Consumer Key
+'XXX',   	// Consumer secret
+'XXX',       // Access token
+'XXX'    	// Access token secret
+);
+
+$twitterData = $twitterConnection->get(
+	'statuses/user_timeline',
+	 array(
+		 'screen_name'     => 'EnglishAssoc',
+		 'count'           => 1,
+		 'exclude_replies' => false
+	)
+);
+
+foreach($twitterData as $tweets)
+{
+     $tweet = $tweets;
+}
+
+echo $tweet->text;
+?>
